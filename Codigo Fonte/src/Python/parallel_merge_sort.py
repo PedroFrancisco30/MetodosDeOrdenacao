7d62d7258895b542d6868e02ee06b69bd3842d32
Paulohenrish <henriquepaulete40@gmail.com>
2026-05-06 09:34:55 -0300

    Add parallel merge sort implementation
    Implement parallel merge sort using threading.

---
+++ Codigo Fonte/src/Python/parallel_merge_sort.py
@@ -0,0 +1,25 @@
+import threading
+
+LIMITE_PARALELO = 3 # Evita explodir threads
+
+from merge_sort import merge
+
+def parallel_merge_sort(array, indx_esq, indx_dir, profundidade):
+    if indx_esq < indx_dir:
+        meio = (indx_esq + indx_dir) // 2
+
+        if profundidade > 0:
+            thread1 = threading.Thread(target=parallel_merge_sort, args=(array, indx_esq, meio, profundidade - 1))
+            thread2 = threading.Thread(target=parallel_merge_sort, args=(array, meio + 1, indx_dir, profundidade - 1))
+
+            thread1.start() # Iniciou em paralelo
+            thread2.start()
+
+            thread1.join() # Espera as threads terminarem
+            thread2.join()
+        else: 
+            parallel_merge_sort(array, indx_esq, meio, 0)
+            parallel_merge_sort(array, meio + 1, indx_dir, 0)
+
+        # Espera as duas acabar para que ai faca o merge
+        merge(array, indx_esq, meio, indx_dir)
