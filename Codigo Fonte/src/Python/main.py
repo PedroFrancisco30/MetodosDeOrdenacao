09127f79f82789e238d0d709b5186c8173a45465
Paulohenrish <henriquepaulete40@gmail.com>
2026-05-07 15:03:11 -0300

    implementação main py

--- Codigo Fonte/src/Python/main.py
+++ Codigo Fonte/src/Python/main.py
@@ -3,14 +3,11 @@ import os
 from merge_sort import merge_sort
 from parallel_merge_sort import parallel_merge_sort, LIMITE_PARALELO
 
-def main():
+def processar_arquivo(caminho_arquivo):
     lista_numeros = []
     tempos_medios_merge = []
     tempos_medios_parallel = []
 
-    caminho_arquivo = "input.dat"
-
-    # Lendo input
     try:
         with open(caminho_arquivo, 'r') as arquivo:
             for linha in arquivo:
@@ -18,7 +15,11 @@ def main():
                 if linha_numeros:
                     lista_numeros.append(linha_numeros)
     except FileNotFoundError:
-        print(f"Erro: Arquivo não encontrado em {caminho_arquivo}")
+        print(f"Erro: Arquivo não encontrado em {caminho_arquivo}\n")
+        return
+
+    if not lista_numeros:
+        print(f"Aviso: O arquivo {caminho_arquivo} está vazio ou com formato inválido.\n")
         return
 
     for lista_numero in lista_numeros:
@@ -29,12 +30,10 @@ def main():
             lista_teste_seq = list(lista_numero)
             lista_teste_paral = list(lista_numero)
 
-            # --- Teste Sequencial ---
             inicio1 = time.time() * 1000
             merge_sort(lista_teste_seq, 0, len(lista_teste_seq) - 1)
             fim1 = time.time() * 1000
 
-            # --- Teste Paralelo ---
             inicio2 = time.time() * 1000
             parallel_merge_sort(lista_teste_paral, 0, len(lista_teste_paral) - 1, LIMITE_PARALELO)
             fim2 = time.time() * 1000
@@ -45,14 +44,25 @@ def main():
         tempos_medios_merge.append(tempo_merge_aux / 15.0)
         tempos_medios_parallel.append(tempo_merge_parallel_aux / 15.0)
 
-    print("Tempos medios apos 15 testes: ")
     print("Metodo - 10^2  /  10^3  /  10^4  /  10^5  /  10^6")
-   
+    
     str_merge = " / ".join([f"{tempo:.2f}" for tempo in tempos_medios_merge])
     str_parallel = " / ".join([f"{tempo:.2f}" for tempo in tempos_medios_parallel])
-   
+    
     print(f"Merge  {str_merge}")
-    print(f"Parall {str_parallel}")
+    print(f"Parall {str_parallel}\n")
+
+def main():
+    # Array contendo os caminhos para os 3 arquivos
+    arquivos_input = [
+        "crescente.dat",
+        "decrescente.dat",
+        "random.dat"
+    ]
+
+    for arquivo in arquivos_input:
+        print(f"--- Processando resultados para: {arquivo} ---")
+        processar_arquivo(arquivo)
 
 if __name__ == "__main__":
     main()
